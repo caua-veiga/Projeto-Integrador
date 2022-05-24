847ae2fd75b67c5236e9d37d2fca945faa628714
Caua Veiga <cauaveiga@gmail.com>
2022-05-24 17:05:25 +0100

    HTTP Example Working

--- Network/main.py
+++ Network/main.py
@@ -1,3 +1,6 @@
+
+import machine
+
 def do_connect():
     '''
     Input must be a tuple: (essid, password)
@@ -18,5 +21,44 @@ def do_connect():
     print('network config:', wlan.ifconfig())
 
 
-if __name__=='__main__':
+def main():
     do_connect()
+
+    pins = [machine.Pin(i, machine.Pin.IN) for i in (0, 2, 4, 5, 12, 13, 14, 15)]
+
+    html = """<!DOCTYPE html>
+    <html>
+        <head> <title>ESP32 Pins</title> </head>
+        <body> <h1>ESP32 Pins</h1>
+            <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
+        </body>
+    </html>
+    """
+
+    import socket
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(1)
+
+    print('listening on', addr)
+
+    while True:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        cl_file = cl.makefile('rwb', 0)
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+        rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+        response = html % '\n'.join(rows)
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(response)
+        cl.close()
+
+
+
+if __name__=='__main__':
+    main()
